029f8e48f48adff75d9cc49aece9324297881701
David Wei <davidhwei@meta.com>
2023-08-10 15:49:37 -0700

    io_uring/zcrx: set pp memory provider for an rx queue
    Set the page pool memory provider for the rx queue configured for zero
    copy to io_uring. Then the rx queue is reset using
    netdev_rx_queue_restart() and netdev core + page pool will take care of
    filling the rx queue from the io_uring zero copy memory provider.
    
    For now, there is only one ifq so its destruction happens implicitly
    during io_uring cleanup.
    
    Reviewed-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: David Wei <dw@davidwei.uk>

--- io_uring/zcrx.h
+++ io_uring/zcrx.h
@@ -5,6 +5,7 @@
 #include <linux/io_uring_types.h>
 #include <linux/socket.h>
 #include <net/page_pool/types.h>
+#include <net/net_trackers.h>
 
 #define IO_ZC_RX_UREF			0x10000
 #define IO_ZC_RX_KREF_MASK		(IO_ZC_RX_UREF - 1)
@@ -36,6 +37,7 @@ struct io_zcrx_ifq {
 	u32				if_rxq;
 
 	struct io_mapped_region		region;
+	netdevice_tracker		netdev_tracker;
 };
 
 #if defined(CONFIG_IO_URING_ZCRX)
