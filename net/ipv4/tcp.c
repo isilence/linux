e370237eb13ae812f7b181d92ea83246621b4b35
Pavel Begunkov <asml.silence@gmail.com>
2024-04-15 19:14:32 +0100

    net: fix readable misaccounting
    Somewhere we left skbs makred unreadable where they are well expected to
    be readable, that makes ssh into qemu fail.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2391,7 +2391,7 @@ static int tcp_recvmsg_dmabuf(struct sock *sk, const struct sk_buff *skb,
 	do {
 		start = skb_headlen(skb);
 
-		if (skb->readable) {
+		if (!skb->unreadable) {
 			err = -ENODEV;
 			goto out;
 		}
@@ -2716,10 +2716,10 @@ found_ok_skb:
 
 		if (!(flags & MSG_TRUNC)) {
 			if (last_copied_dmabuf != -1 &&
-			    last_copied_dmabuf != !skb->readable)
+			    last_copied_dmabuf != skb->unreadable)
 				break;
 
-			if (skb->readable) {
+			if (!skb->unreadable) {
 				err = skb_copy_datagram_msg(skb, offset, msg,
 							    used);
 				if (err) {
@@ -2751,7 +2751,7 @@ found_ok_skb:
 			}
 		}
 
-		last_copied_dmabuf = !skb->readable;
+		last_copied_dmabuf = skb->unreadable;
 
 		WRITE_ONCE(*seq, *seq + used);
 		copied += used;
