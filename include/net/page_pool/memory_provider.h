8ecd149200079b6dcf7bee09883da9edde77e2bc
Pavel Begunkov <asml.silence@gmail.com>
2024-10-16 00:55:51 +0100

    net: page_pool: introduce page_pool_mp_return_in_cache
    Add a helper that allows a page pool memory provider to efficiently
    return a netmem off the allocation callback.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- include/net/page_pool/memory_provider.h
+++ include/net/page_pool/memory_provider.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 #ifndef _NET_PAGE_POOL_MEMORY_PROVIDER_H
 #define _NET_PAGE_POOL_MEMORY_PROVIDER_H
 
@@ -6,5 +8,7 @@ int page_pool_mp_init_paged_area(struct page_pool *pool,
 				struct page **pages);
 void page_pool_mp_release_area(struct page_pool *pool,
 				struct net_iov_area *area);
+
+void page_pool_mp_return_in_cache(struct page_pool *pool, netmem_ref netmem);
 
 #endif
