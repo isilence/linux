e3c8b1fbf53b8fa2c08d7a224f19928371862865
Pavel Begunkov <asml.silence@gmail.com>
2024-12-11 17:33:09 +0000

    net: page_pool: add a mp hook to unregister_netdevice*
    Devmem TCP needs a hook in unregister_netdevice_many_notify() to upkeep
    the set tracking queues it's bound to, i.e. ->bound_rxqs. Instead of
    devmem sticking directly out of the genetic path, add a mp function.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- include/net/page_pool/memory_provider.h
+++ include/net/page_pool/memory_provider.h
@@ -19,6 +19,7 @@ struct memory_provider_ops {
 	void (*destroy)(struct page_pool *pool);
 	int (*nl_fill)(void *mp_priv, struct sk_buff *rsp,
 		       struct netdev_rx_queue *rxq);
+	void (*uninstall)(void *mp_priv, struct netdev_rx_queue *rxq);
 };
 
 #endif
