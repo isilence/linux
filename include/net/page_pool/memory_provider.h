6691fe91a53116ab617c7ec743e2d278d2802bb2
Pavel Begunkov <asml.silence@gmail.com>
2024-12-11 15:10:19 +0000

    net: page_pool: add mp op for netlink reporting
    Add a mandatory memory provider callback that prints information about
    the provider.
    
    Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- include/net/page_pool/memory_provider.h
+++ include/net/page_pool/memory_provider.h
@@ -9,11 +9,14 @@
 #include <net/netmem.h>
 #include <net/page_pool/types.h>
 
+struct sk_buff;
+
 struct memory_provider_ops {
 	netmem_ref (*alloc_netmems)(struct page_pool *pool, gfp_t gfp);
 	bool (*release_netmem)(struct page_pool *pool, netmem_ref netmem);
 	int (*init)(struct page_pool *pool);
 	void (*destroy)(struct page_pool *pool);
+	int (*nl_report)(const struct page_pool *pool, struct sk_buff *rsp);
 };
 
 #endif
