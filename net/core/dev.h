0ac4e47001e1e7adea755a3c45552079104549b9
Jakub Kicinski <kuba@kernel.org>
2025-04-21 15:28:23 -0700

    net: wipe the setting of deactived queues
    Clear out all settings of deactived queues when user changes
    the number of channels. We already perform similar cleanup
    for shapers.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- net/core/dev.h
+++ net/core/dev.h
@@ -101,6 +101,8 @@ void __netdev_queue_config(struct net_device *dev, int rxq,
 			   struct netdev_queue_config *qcfg, bool pending);
 int netdev_queue_config_revalidate(struct net_device *dev,
 				   struct netlink_ext_ack *extack);
+void netdev_queue_config_update_cnt(struct net_device *dev, unsigned int txq,
+				    unsigned int rxq);
 
 /* netdev management, shared between various uAPI entry points */
 struct netdev_name_node {
