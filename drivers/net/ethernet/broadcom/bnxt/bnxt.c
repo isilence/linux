8eb9793e5bd1100dd6db95e0c84cac32f8b44152
Jakub Kicinski <kuba@kernel.org>
2025-04-21 15:28:19 -0700

    eth: bnxt: always set the queue mgmt ops
    Core provides a centralized callback for validating per-queue settings
    but the callback is part of the queue management ops. Having the ops
    conditionally set complicates the parts of the driver which could
    otherwise lean on the core to feel it the correct settings.
    
    Always set the queue ops, but provide no restart-related callbacks if
    queue ops are not supported by the device. This should maintain current
    behavior, the check in netdev_rx_queue_restart() looks both at op struct
    and individual ops.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -16126,6 +16126,9 @@ static const struct netdev_queue_mgmt_ops bnxt_queue_mgmt_ops = {
 	.ndo_queue_stop		= bnxt_queue_stop,
 };
 
+static const struct netdev_queue_mgmt_ops bnxt_queue_mgmt_ops_unsupp = {
+};
+
 static void bnxt_remove_one(struct pci_dev *pdev)
 {
 	struct net_device *dev = pci_get_drvdata(pdev);
@@ -16781,7 +16784,8 @@ static int bnxt_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 		bp->rss_cap |= BNXT_RSS_CAP_MULTI_RSS_CTX;
 	if (BNXT_SUPPORTS_QUEUE_API(bp))
 		dev->queue_mgmt_ops = &bnxt_queue_mgmt_ops;
-	dev->request_ops_lock = true;
+	else
+		dev->queue_mgmt_ops = &bnxt_queue_mgmt_ops_unsupp;
 
 	rc = register_netdev(dev);
 	if (rc)
