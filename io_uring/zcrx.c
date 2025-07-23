a0ec07a0e0d50973c43ce22150ec4029132ab389
Pavel Begunkov <asml.silence@gmail.com>
2025-07-23 11:42:27 +0100

    io_uring/zcrx: implement large rx buffer support
    There are network cards that support receive buffers larger than 4K, and
    that can be vastly beneficial for performance, and benchmarks for this
    patch showed up to 30% CPU util improvement for 32K vs 4K buffers.
    
    Allows zcrx users to specify the size in struct
    io_uring_zcrx_ifq_reg::rx_buf_len. If set to zero, zcrx will use a
    default value. zcrx will check and fail if the memory backing the area
    can't be split into physically contiguous chunks of the required size.
    It's more restrictive as it only needs dma addresses to be contig, but
    that's beyond this series.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- io_uring/zcrx.c
+++ io_uring/zcrx.c
@@ -13,6 +13,7 @@
 #include <net/page_pool/memory_provider.h>
 #include <net/netlink.h>
 #include <net/netdev_rx_queue.h>
+#include <net/netdev_queues.h>
 #include <net/tcp.h>
 #include <net/rps.h>
 
@@ -51,6 +52,18 @@ static inline struct page *io_zcrx_iov_page(const struct net_iov *niov)
 
 	niov_pages_shift = area->ifq->niov_shift - PAGE_SHIFT;
 	return area->mem.pages[net_iov_idx(niov) << niov_pages_shift];
+}
+
+static int io_area_max_shift(struct io_zcrx_mem *mem)
+{
+	struct sg_table *sgt = mem->sgt;
+	struct scatterlist *sg;
+	unsigned order = -1U;
+	unsigned i;
+
+	for_each_sgtable_dma_sg(sgt, sg, i)
+		order = min(order, __ffs(sg->length));
+	return order;
 }
 
 static int io_populate_area_dma(struct io_zcrx_ifq *ifq,
@@ -384,8 +397,10 @@ static int io_zcrx_append_area(struct io_zcrx_ifq *ifq,
 }
 
 static int io_zcrx_create_area(struct io_zcrx_ifq *ifq,
-			       struct io_uring_zcrx_area_reg *area_reg)
+			       struct io_uring_zcrx_area_reg *area_reg,
+			       struct io_uring_zcrx_ifq_reg *reg)
 {
+	int buf_size_shift = PAGE_SHIFT;
 	struct io_zcrx_area *area;
 	unsigned nr_iovs;
 	int i, ret;
@@ -400,7 +415,16 @@ static int io_zcrx_create_area(struct io_zcrx_ifq *ifq,
 	if (ret)
 		goto err;
 
-	ifq->niov_shift = PAGE_SHIFT;
+	if (reg->rx_buf_len) {
+		if (!is_power_of_2(reg->rx_buf_len) ||
+		     reg->rx_buf_len < PAGE_SIZE)
+			return -EINVAL;
+		buf_size_shift = ilog2(reg->rx_buf_len);
+	}
+	if (buf_size_shift > io_area_max_shift(&area->mem))
+		return -EINVAL;
+
+	ifq->niov_shift = buf_size_shift;
 	nr_iovs = area->mem.size >> ifq->niov_shift;
 	area->nia.num_niovs = nr_iovs;
 
@@ -544,8 +568,7 @@ int io_register_zcrx_ifq(struct io_ring_ctx *ctx,
 		return -EFAULT;
 	if (copy_from_user(&rd, u64_to_user_ptr(reg.region_ptr), sizeof(rd)))
 		return -EFAULT;
-	if (!mem_is_zero(&reg.__resv, sizeof(reg.__resv)) ||
-	    reg.__resv2 || reg.zcrx_id)
+	if (!mem_is_zero(&reg.__resv, sizeof(reg.__resv)) || reg.zcrx_id)
 		return -EINVAL;
 	if (reg.if_rxq == -1 || !reg.rq_entries || reg.flags)
 		return -EINVAL;
@@ -589,12 +612,13 @@ int io_register_zcrx_ifq(struct io_ring_ctx *ctx,
 	}
 	get_device(ifq->dev);
 
-	ret = io_zcrx_create_area(ifq, &area);
+	ret = io_zcrx_create_area(ifq, &area, &reg);
 	if (ret)
 		goto err;
 
 	mp_param.mp_ops = &io_uring_pp_zc_ops;
 	mp_param.mp_priv = ifq;
+	mp_param.rx_buf_len = 1U << ifq->niov_shift;
 	ret = net_mp_open_rxq(ifq->netdev, reg.if_rxq, &mp_param);
 	if (ret)
 		goto err;
@@ -611,6 +635,8 @@ int io_register_zcrx_ifq(struct io_ring_ctx *ctx,
 		if (xa_store(&ctx->zcrx_ctxs, id, ifq, GFP_KERNEL))
 			goto err;
 	}
+
+	reg.rx_buf_len = 1U << ifq->niov_shift;
 
 	if (copy_to_user(arg, &reg, sizeof(reg)) ||
 	    copy_to_user(u64_to_user_ptr(reg.region_ptr), &rd, sizeof(rd)) ||
