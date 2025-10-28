661ca8360cfe00533cf21e16309d6f873cc73d95
David Wei <dw@davidwei.uk>
2025-10-28 10:46:38 -0700

    io_uring/zcrx: add refcount to ifq and remove ifq->ctx
    Add a refcount to struct io_zcrx_ifq to track the number of rings that
    share it. For now, this is only ever 1 i.e. not shared.
    
    This refcount replaces the ref that the ifq holds on ctx->refs via the
    page pool memory provider. This was used to keep the ifq around until
    the ring ctx is being freed i.e. ctx->refs fall to 0. But with ifq now
    being refcounted directly by the ring, and ifq->ctx removed, this is no
    longer necessary.
    
    Since ifqs now no longer hold refs to ring ctx, there isn't a need to
    split the cleanup of ifqs into two: io_shutdown_zcrx_ifqs() in
    io_ring_exit_work() while waiting for ctx->refs to drop to 0, and
    io_unregister_zcrx_ifqs() after. Remove io_shutdown_zcrx_ifqs().
    
    So an ifq now behaves like a normal refcounted object; the last ref from
    a ring will free the ifq.
    
    Signed-off-by: David Wei <dw@davidwei.uk>
    [pavel: shutdown ifq on unregister, make pp to pin zcrx]
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- io_uring/zcrx.h
+++ io_uring/zcrx.h
@@ -39,7 +39,6 @@ struct io_zcrx_area {
 };
 
 struct io_zcrx_ifq {
-	struct io_ring_ctx		*ctx;
 	struct io_zcrx_area		*area;
 	unsigned			niov_shift;
 	struct user_struct		*user;
@@ -55,6 +54,7 @@ struct io_zcrx_ifq {
 	struct device			*dev;
 	struct net_device		*netdev;
 	netdevice_tracker		netdev_tracker;
+	refcount_t			refs;
 
 	/*
 	 * Page pool and net configuration lock, can be taken deeper in the
@@ -69,7 +69,6 @@ int io_zcrx_ctrl(struct io_ring_ctx *ctx, void __user *arg, unsigned nr_arg);
 int io_register_zcrx_ifq(struct io_ring_ctx *ctx,
 			 struct io_uring_zcrx_ifq_reg __user *arg);
 void io_unregister_zcrx_ifqs(struct io_ring_ctx *ctx);
-void io_shutdown_zcrx_ifqs(struct io_ring_ctx *ctx);
 int io_zcrx_recv(struct io_kiocb *req, struct io_zcrx_ifq *ifq,
 		 struct socket *sock, unsigned int flags,
 		 unsigned issue_flags, unsigned int *len);
@@ -82,9 +81,6 @@ static inline int io_register_zcrx_ifq(struct io_ring_ctx *ctx,
 	return -EOPNOTSUPP;
 }
 static inline void io_unregister_zcrx_ifqs(struct io_ring_ctx *ctx)
-{
-}
-static inline void io_shutdown_zcrx_ifqs(struct io_ring_ctx *ctx)
 {
 }
 static inline int io_zcrx_recv(struct io_kiocb *req, struct io_zcrx_ifq *ifq,
